1c3e268cb1410663171aa2ed0971e9be2cee338e
Fernando Ribas <fribas84@gmail.com>
2026-09-20 19:56:32 -0300

    Update fundraiser program ID and localnet configuration

--- programs/fundraiser/src/lib.rs
+++ programs/fundraiser/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("7WferfAMCt6f32DYucuQNhnSYdoV7SWSR92od8t1jDzW");
+declare_id!("Gas8wW4Yk4DDfc99pB9A8gSHSZiDpXNaENQkC2wGdwqU");
 
 mod state;
 mod instructions;
